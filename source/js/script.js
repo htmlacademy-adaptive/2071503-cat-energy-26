4efbd05d4e52602329971b2aa51bb1cfbdbca7db
Vladislav <vladv505653@gmail.com>
2022-09-15 15:41:08 +0300

    защита

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,11 @@
 const hamb = document.querySelector('.nav__toggle');
 const menu = document.querySelector('.nav__list');
 
+// убираем классы при включеном js
+menu.classList.remove('nav__list--no-js');
+hamb.classList.remove('nav__toggle--no-js');
+
 hamb.addEventListener('click', function(){
-    menu.classList.toggle('active');
-    hamb.classList.toggle('active');
+    menu.classList.toggle('nav__list--active');
+    hamb.classList.toggle('nav__toggle--active');
 });
